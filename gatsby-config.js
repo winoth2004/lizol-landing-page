618398b8137a5f8c090053ca86a33747df2d51cd
Vinoth Balaji Kannan <vinotb@amazon.com>
2020-11-12 21:32:23 -0800

    Removing optional analytics confing properties

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,14 +37,6 @@ module.exports = {
         trackingId: process.env.GATSBY_GOOGLE_TRACKING_ID,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
         optimizeId: process.env.GATSBY_GOOGLE_OPTIMIZE_ID,
         // Enables Google Optimize Experiment ID
@@ -53,9 +45,6 @@ module.exports = {
         variationId: "0",
         // Defers execution of google analytics script after page load
         defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
         cookieDomain: process.env.GATSBY_WEB_DOMAIN,
       },
     },
