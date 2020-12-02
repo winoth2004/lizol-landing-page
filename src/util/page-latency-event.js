b09800602f185071c6cbc20d9a3ac8abb4602024
Vinoth Balaji Kannan <vinotb@amazon.com>
2020-12-02 13:50:13 -0800

    Adding first google analytics event

--- src/util/page-latency-event.js
+++ src/util/page-latency-event.js
@@ -1,5 +1,21 @@
 export function initPageEvent() {
-    if(document) {
-        console.log(`DOM Content loaded time is ${(new Date()) - window.t0}`);
+    if(window && document) {
+        postMetrics('timing_complete', {
+            'name'           : 'page_component_load',
+            'value'          : (window.t0 ? ((new Date()) - window.t0) : 0),
+            'event_category' : 'JS Dependencies'
+        })
     }
 }
+
+const postMetrics = (function() {
+    if (window && gtag) {
+        return ((action, parameterObject) => {
+            gtag('event', action, parameterObject);
+        });
+    } else {
+        return (() => {
+            console.log(`Post metrics arguments ${JSON.stringify(arguments)}`);
+        });
+    }
+})();
