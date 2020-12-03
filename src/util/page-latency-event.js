aa5c31f374f010672db323cc057eb2e2d22fa75b
Vinoth Balaji Kannan <vinotb@amazon.com>
2020-12-03 02:36:22 -0800

    Adding PerformanceObserver log

--- src/util/page-latency-event.js
+++ src/util/page-latency-event.js
@@ -1,15 +1,23 @@
 export function initPageEvent() {
     if(window && document) {
-        postMetrics('metrics', {
+        postMetrics('timing_complete', {
             'name'           : 'page_component_load',
-            'value'          : (window.t0 ? ((new Date()) - window.t0) : 0)
-        })
+            'value'          : (window.t0 ? ((new Date()) - window.t0) : 0),
+            'event_category' : 'JS Dependencies'
+        });
+
+        var performanceObserver = new PerformanceObserver((performanceEntries, performanceObserver) => {
+            performanceEntries.getEntries().forEach(entry => {
+                console.log(`Name is ${entry.name} and startTime is ${entry.startTime}`);
+            });
+        });
+        performanceObserver.observe({ type: ['paint'] });
     }
 }
 
 const postMetrics = function(action, parameterObject) {
-    if (window && gtag) {
-        gtag('event', action, parameterObject);
+    if (window && window.gtag) {
+        window.gtag('event', action, parameterObject);
     } else {
         console.log(`Post metrics arguments ${JSON.stringify(arguments)}`);
     }
